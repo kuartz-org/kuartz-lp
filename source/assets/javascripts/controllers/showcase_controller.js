88c97744f2fdb2051ced9bae5034972ed9a19c9a
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2020-05-08 18:46:36 +0200

    showcase with mobile

--- source/assets/javascripts/controllers/showcase_controller.js
+++ source/assets/javascripts/controllers/showcase_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "stimulus";
 
 export default class extends Controller {
   static get targets() {
-    return [ "image", "link" ]
+    return [ "image", "imageMobile", "link", "showMobile" ]
   }
 
   initialize() {
@@ -13,6 +13,24 @@ export default class extends Controller {
     event.preventDefault();
 
     this.imageTarget.setAttribute('src', event.target.dataset.image);
+
+    this.showMobileTarget.classList.add('hidden');
+
+    this.linkTargets.forEach((link) => {
+      link.classList.remove('bg-gray-400');
+      link.classList.replace('text-gray-700', 'text-gray-500');
+      link.classList.add('hover:text-gray-600');
+    })
+
+    this.setActive(event.currentTarget);
+  }
+
+  displayImageWithMobile(event) {
+    event.preventDefault();
+
+    this.imageTarget.setAttribute('src', event.target.dataset.image);
+    this.imageMobileTarget.setAttribute('src', event.target.dataset.imageMobile);
+    this.showMobileTarget.classList.remove('hidden');
 
     this.linkTargets.forEach((link) => {
       link.classList.remove('bg-gray-400');
