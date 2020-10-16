5228c3449e847fe08a35ae3c64f3c67324abee8f
Timothée Garcia <grc.tmt@gmail.com>
2020-10-16 10:29:46 +0200

    remove crisp from lp

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -5,14 +5,14 @@ const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
 
-document.addEventListener('DOMContentLoaded', (event) => {
-  const openCrispButtons = document.querySelectorAll('.open-crisp');
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   const openCrispButtons = document.querySelectorAll('.open-crisp');
 
-  openCrispButtons.forEach((button) => {
-    button.addEventListener('click', (event) => {
-      event.preventDefault();
+//   openCrispButtons.forEach((button) => {
+//     button.addEventListener('click', (event) => {
+//       event.preventDefault();
 
-      $crisp.push(['do', 'chat:open']);
-    })
-  })
-})
+//       $crisp.push(['do', 'chat:open']);
+//     })
+//   })
+// })
